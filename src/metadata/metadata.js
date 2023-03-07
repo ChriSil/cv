25cf4e008122930d1db19938ec8c6312e29e12ec
Christopher Siller <chsiller91@gmail.com>
2023-03-07 20:34:57 +0100

    6th update

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,13 +1,14 @@
 module.exports = {
-  name: 'William Shakespeare',
+  name: 'Christopehr Siller',
   title: 'Software Engineer, M.Sc.',
   facts: {
       'Residence': '<a href="https://www.google.com/maps/place/Innsbruck/@47.2855079,11.3437708,13z/data=!3m1!4b1!4m6!3m5!1s0x479d6ecfe1f8ca73:0x9d201c7d281d9b0d!8m2!3d47.2692124!4d11.4041024!16zL20vMGZobXk"><i class="fa fa-home fact-icon"></i>Innsbruck, AT</a>',
       'LinkedIn': `<a href="https://www.linkedin.com/in/christophersiller/en?original_referer=https%3A%2F%2Fwww.google.com%2F">
                   <i class="fa-brands fa-linkedin-in fact-icon"></i>Christopher Siller
                 </a>`,
-    'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
-    'Email': '<a href="mailto:chris.siller@alumni.fau.de"><i class="fa fa-envelope fact-icon"></i>chris.siller@alumni.fau.de</a>'
+      'Github': `<a href="https://github.com/ChriSil/"><i class="fa-brands fa-github fact-icon"></i>ChriSil</a>`,
+      'Email': '<a href="mailto:chris.siller@alumni.fau.de"><i class="fa fa-envelope fact-icon"></i>chris.siller@alumni.fau.de</a>'
+      'Phone': '<a href="tel:>+43-670-3522557"><i class="fa fa-envelope fact-icon"></i>+43 670 3522557</a>'
   },
   skills: [
     ['Python', 100],
@@ -35,7 +36,7 @@ module.exports = {
     {
         title: 'Hatch Ltd.',
         subtitle: 'Software and Systems Engineer',
-        period: 'October 2018 – October 2021',
+        period: 'Oct 2018 – Oct 2021',
         skills: ['C++', 'Python', 'MatLab'],
         contents: `Part of the Rail Systems Engineering group.    Responsibilities revolved mainly around
         development and application of TrainOps\u00A9 , Hatch LTKs proprietary operations and electrical
@@ -47,9 +48,9 @@ module.exports = {
     },
     {
         title: 'Siemens Transmission Solutions,',
+        period: 'Nov 2015 – Dec 2017',
         subtitle: 'Erlangen, Germany',
         jtitle: 'Working Student, R&D',
-        written: 'November 2015 - December 2017',
         badges: ['LabView', 'C++', 'MatLab'],
         contents: `R&D Team member,  building a test facility for a high voltage DC Switch. My task was to build a virtual workbench to allow remote charging, testing and monitoring, as well as test result analysis. `
     },
@@ -57,7 +58,7 @@ module.exports = {
         title: 'Siemens Rail Electrification,',
         subtitle: 'Portland, OR, USA',
         jtitle: 'Trainee, Project Engineering',
-        written: 'October 2014 - July 2015',
+        period: 'Oct 2014 - Jul 2015',
         badges: ['LabView', 'MatLab', 'Siemens NX'],
         contents: `Introduction to technical project management. Assisted in installing inspecting DC Substations. Facilitated automated Factory Acceptance Test (FAT) workbench for Rectifiers and DC Switchgear. `,
         subcontents: []
@@ -66,7 +67,7 @@ module.exports = {
         title: 'Siemens Infrastructure and Cities,',
         subtitle: 'Erlangen, Germany',
         jtitle: 'Working Student, IT Infrastructure',
-        written: 'June 2012 - September 2014',
+        period: 'Jun 2012 - Sep 2014',
         badges: ['SQL', 'Linux', 'VBA', 'SharePoint', 'Access'],
         contents: 'Working Student in part time, helping on-site with user IT issues, migration of Operating Systems, maintenance and migration of databases and server environments.'
     }
@@ -74,18 +75,18 @@ module.exports = {
  experience: [
           {
               header: 'Masters Thesis',
-              skills: ['theater', 'play'],
+              skills: ['C++', 'Python', 'ROS'],
               contents: `Software development to calculating flight trajectories for autonomous
       multicopters in intralogistics`
           },
           {
               header: 'Project Thesis',
-              skills: ['theater', 'play'],
+              skills: ['3d Print', 'Aerosols'],
               contents: `Fabrication of artficial muscles using the Aerosol-Jet-3D-Print method`
           },
           {
               header: 'Bachelors Thesis',
-              skills: ['theater', 'play'],
+              skills: ['FEM Analysis', 'Thermal Condictivity'],
               contents: `Experimental and simulative analysis of thermally conductive polymers in
       high power LED-systems`
           },
