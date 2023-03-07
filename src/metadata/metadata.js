fa1d479ca4d4f1e4267940ece7e820cdd3ac2bd1
Christopher Siller <chsiller91@gmail.com>
2023-03-07 20:17:05 +0100

    5th update

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -4,7 +4,7 @@ module.exports = {
   facts: {
       'Residence': '<a href="https://www.google.com/maps/place/Innsbruck/@47.2855079,11.3437708,13z/data=!3m1!4b1!4m6!3m5!1s0x479d6ecfe1f8ca73:0x9d201c7d281d9b0d!8m2!3d47.2692124!4d11.4041024!16zL20vMGZobXk"><i class="fa fa-home fact-icon"></i>Innsbruck, AT</a>',
       'LinkedIn': `<a href="https://www.linkedin.com/in/christophersiller/en?original_referer=https%3A%2F%2Fwww.google.com%2F">
-                  <i class="fa-brands fa-wikipedia-w fact-icon"></i>Christopher Siller
+                  <i class="fa-brands fa-linkedin-in fact-icon"></i>Christopher Siller
                 </a>`,
     'Twitter': `<a href="https://twitter.com/shakespeare"><i class="fa-brands fa-twitter fact-icon"></i>Shakespeare</a>`,
     'Email': '<a href="mailto:chris.siller@alumni.fau.de"><i class="fa fa-envelope fact-icon"></i>chris.siller@alumni.fau.de</a>'
@@ -46,28 +46,48 @@ module.exports = {
 
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-
-    }
-  ],
-  experience: [
-    {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+        title: 'Siemens Transmission Solutions,',
+        subtitle: 'Erlangen, Germany',
+        jtitle: 'Working Student, R&D',
+        written: 'November 2015 - December 2017',
+        badges: ['LabView', 'C++', 'MatLab'],
+        contents: `R&D Team member,  building a test facility for a high voltage DC Switch. My task was to build a virtual workbench to allow remote charging, testing and monitoring, as well as test result analysis. `
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
+        title: 'Siemens Rail Electrification,',
+        subtitle: 'Portland, OR, USA',
+        jtitle: 'Trainee, Project Engineering',
+        written: 'October 2014 - July 2015',
+        badges: ['LabView', 'MatLab', 'Siemens NX'],
+        contents: `Introduction to technical project management. Assisted in installing inspecting DC Substations. Facilitated automated Factory Acceptance Test (FAT) workbench for Rectifiers and DC Switchgear. `,
+        subcontents: []
+    },
+    {
+        title: 'Siemens Infrastructure and Cities,',
+        subtitle: 'Erlangen, Germany',
+        jtitle: 'Working Student, IT Infrastructure',
+        written: 'June 2012 - September 2014',
+        badges: ['SQL', 'Linux', 'VBA', 'SharePoint', 'Access'],
+        contents: 'Working Student in part time, helping on-site with user IT issues, migration of Operating Systems, maintenance and migration of databases and server environments.'
     }
+  ],
+ experience: [
+          {
+              header: 'Masters Thesis',
+              skills: ['theater', 'play'],
+              contents: `Software development to calculating flight trajectories for autonomous
+      multicopters in intralogistics`
+          },
+          {
+              header: 'Project Thesis',
+              skills: ['theater', 'play'],
+              contents: `Fabrication of artficial muscles using the Aerosol-Jet-3D-Print method`
+          },
+          {
+              header: 'Bachelors Thesis',
+              skills: ['theater', 'play'],
+              contents: `Experimental and simulative analysis of thermally conductive polymers in
+      high power LED-systems`
+          },
   ]
 };
